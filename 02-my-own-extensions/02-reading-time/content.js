322e0e236ff4ea2eb4d04e9286539b82c317896e
Richard Alves <54530477+richarddalves@users.noreply.github.com>
2025-05-27 01:37:55 -0300

    logic for reading time

--- 02-my-own-extensions/02-reading-time/content.js
+++ 02-my-own-extensions/02-reading-time/content.js
@@ -1,5 +1,14 @@
-// Procura o primeiro h1 que aparece na página
+// Searchs for the first h1 on the page
 const h1 = document.querySelector("h1");
 
-if (h1) console.log(`Title found: ${h1.innerText}!`);
+const h1Text = h1.innerText;
+const h1Words = h1Text.trim().split(/\s+/);
+const h1CountWord = h1Words.length;
+
+let wordsPerMinut = 200;
+
+const minutes = h1CountWord / wordsPerMinut;
+const minutesRounded = Math.ceil(minutes * 100) / 100;
+
+if (h1) console.log(`Estimated reading time for the title: ${minutesRounded} minutes!`);
 else console.warn("No <h1> was found in this page!");
